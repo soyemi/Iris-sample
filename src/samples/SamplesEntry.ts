d5e0844d76aa697e3265fb440c623073c6495556
yemi <zcyemi@gmail.com>
2018-12-25 09:21:25 +0800

    add shadowmap sample.

--- src/samples/SamplesEntry.ts
+++ src/samples/SamplesEntry.ts
@@ -2,10 +2,12 @@ import { CubeSample } from './basic/CubeSample';
 import { PathTracer } from "./path-tracer/PathTracer";
 import { GLTFSample } from './gltf/GLTFSample';
 import { SkyboxSample } from './skybox/SkyboxSample';
+import { ShadowmapSample } from './shadowmap/ShadowmapSample';
 
 export const SAMPLES_ENTRY = {
     "skybox":SkyboxSample,
     "gltf-assets":GLTFSample,
     "cube":CubeSample,
-    "path-tracer":PathTracer
+    "path-tracer":PathTracer,
+    "shadowmap":ShadowmapSample,
 }
---
+++ src/samples/shadowmap/ShadowmapSamplePipeline.ts
@@ -0,0 +1,34 @@
+import * as iris from 'iris-gl';
+
+export class ShadowmapSamplePipeline extends iris.PipelineBase{
+ 
+    private m_passDepth:iris.PassDepth;
+    private m_passOpaque:iris.PassOpaque;
+
+    
+    public async init(){
+        if(this.m_inited) return;
+        super.init();
+        
+        this.m_passDepth = new iris.PassDepth(this);
+        this.m_passOpaque = new iris.PassOpaque(this);
+    }
+    
+
+    public exec(scene:iris.Scene){
+        if(scene == null) return;
+
+        this.generateDrawList(scene);
+        
+        this.m_passDepth.render(scene);
+        this.m_passOpaque.render(scene);
+
+    }
+
+    public release(){
+        super.release();
+
+        this.m_passDepth = iris.RenderPass.Release(this.m_passDepth);
+        this.m_passOpaque = iris.RenderPass.Release(this.m_passOpaque);
+    }
+}
